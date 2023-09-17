d5bdf3111616da780d17cfa4684af31aa350219e
LoganKells <lkells@outlook.com>
2023-09-17 06:47:28 -0400

    Added another example

--- course-2-progamming-with-javascript/javascript-examples/src/arrays/arrayPrototypeMap.js
+++ course-2-progamming-with-javascript/javascript-examples/src/arrays/arrayPrototypeMap.js
@@ -18,4 +18,21 @@ const data = [
 const topDesserts = data.map((desert) => {
     return { content: `${desert.title} - ${desert.description}` }
 })
-console.log(topDesserts)
+console.log('topDesserts: ', topDesserts)
+
+const desserts = [
+    {
+        title: 'Chocolate Cake',
+        description: 'Chocolate cake is a cake flavored with melted chocolate',
+        calories: 500,
+    },
+]
+
+const newDesserts = desserts.map((dessert) => {
+    return {
+        title: dessert.title.toUpperCase(),
+        ...dessert, // NOTE - this replaces the value of title in the object literal.
+        kCal: dessert.calories / 1000,
+    }
+})
+console.log('newDesserts: ', newDesserts)
