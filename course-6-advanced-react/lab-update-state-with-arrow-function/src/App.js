e6b19d488cebfe959fdb8d434d37121e511db8f5
LoganKells <lkells@outlook.com>
2023-09-27 12:48:54 -0400

    Created lab-update-state-with-arrow-function using create-react-app

---
+++ course-6-advanced-react/lab-update-state-with-arrow-function/src/App.js
@@ -0,0 +1,36 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [data, setData] = useState({ id: 0, name: "Logan", lastName: "Kells" });
+  let [clickCount, setClickCount] = useState(0);
+  function updateData() {
+    setClickCount(++clickCount);
+    setData((prevValue) => {
+      return { ...prevValue, id: clickCount };
+    });
+  }
+  console.log(data);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <button onClick={updateData}>Update Data</button>
+    </div>
+  );
+}
+
+export default App;
