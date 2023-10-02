ce211d157ed7af30b33f38879b22c77212efe108
LoganKells <lkells@outlook.com>
2023-10-02 12:38:56 -0400

    Display user first name

--- course-6-advanced-react/lab-fetching-and-rendering-data/src/App.js
+++ course-6-advanced-react/lab-fetching-and-rendering-data/src/App.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function App() {
   const [user, setUser] = React.useState([]);
 
   const fetchData = () => {
-
+    fetch("https://randomuser.me/api/?results=1")
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data["results"][0]["name"]["first"]);
+      })
+      .catch((error) => console.log(error));
   };
 
   React.useEffect(() => {
     fetchData();
   }, []);
 
-  return Object.keys(user).length > 0 ? (
-    <div style={{padding: "40px"}}>
-      <h1>Customer data</h1>
+  useEffect(() => {
+    console.log("user:", user);
+    // console.log("name:", user[0]["name"]["first"]);
+  }, [user]);
 
+  // conditional rendering
+  return Object.keys(user).length > 0 ? (
+    <div style={{ padding: "40px" }}>
+      <h1>Customer data</h1>
+      <h2>Name: {user}</h2>
     </div>
   ) : (
     <h1>Data pending...</h1>
