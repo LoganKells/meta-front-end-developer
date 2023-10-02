4b2d02bd42dca321639c51bdb6422476948b4892
LoganKells <lkells@outlook.com>
2023-10-02 14:34:04 -0400

    use object literal

--- course-6-advanced-react/lab-fetching-and-rendering-data/src/App.js
+++ course-6-advanced-react/lab-fetching-and-rendering-data/src/App.js
@@ -1,31 +1,38 @@
 import React, { useEffect } from "react";
 
 function App() {
-  const [user, setUser] = React.useState([]);
+  const [users, setUsers] = React.useState([]);
 
   const fetchData = () => {
     fetch("https://randomuser.me/api/?results=1")
       .then((response) => response.json())
       .then((data) => {
-        setUser(data["results"][0]["name"]["first"]);
+        setUsers(data);
       })
       .catch((error) => console.log(error));
   };
 
+  // Only call once
   React.useEffect(() => {
     fetchData();
   }, []);
 
   useEffect(() => {
-    console.log("user:", user);
-    // console.log("name:", user[0]["name"]["first"]);
-  }, [user]);
+    console.log("user:", users);
+  }, [users]);
 
   // conditional rendering
-  return Object.keys(user).length > 0 ? (
+  return Object.keys(users).length > 0 ? (
     <div style={{ padding: "40px" }}>
       <h1>Customer data</h1>
-      <h2>Name: {user}</h2>
+      <h2>
+        Name: {users.results[0].name.first} {users.results[0].name.last}
+      </h2>
+      <img
+        src={users.results[0].picture.large}
+        alt={"User Image"}
+        width={200}
+      />
     </div>
   ) : (
     <h1>Data pending...</h1>
