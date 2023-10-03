010e9a9ceeb867e9094153b6ce48cfc8520ffa7d
LoganKells <lkells@outlook.com>
2023-10-03 14:44:57 -0400

    useRef to edit the focus of html input element.

--- course-6-advanced-react/lab-edit-properties-of-specific-dom-node/src/App.tsx
+++ course-6-advanced-react/lab-edit-properties-of-specific-dom-node/src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import "./App.css";
+import { Simulate } from "react-dom/test-utils";
 
 function App() {
+  const formInputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>{"Lab to access <input> in underlying DOM."}</h1>
+        <input ref={formInputRef} type="text" />
+        <button onClick={() => formInputRef.current?.focus()}>
+          Focus cursor to Input
+        </button>
+      </div>
+    </>
   );
 }
 
