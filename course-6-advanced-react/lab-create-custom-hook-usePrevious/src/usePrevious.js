67a6acbf774fd6eb2c7123be98f7d398107a9d23
LoganKells <lkells@outlook.com>
2023-10-03 15:21:17 -0400

    Created docstring

--- course-6-advanced-react/lab-create-custom-hook-usePrevious/src/usePrevious.js
+++ course-6-advanced-react/lab-create-custom-hook-usePrevious/src/usePrevious.js
@@ -1,5 +1,11 @@
 import { useEffect, useRef } from "react";
 
+/**
+ * usePrevious is a custom hook. The value input is stored in an object,
+ * prevVal, and returned when the hook is called. The hook is called
+ * anytime the val value changes in the component because useEffect is
+ * observing the dependency [val].
+ * */
 function usePrevious(val) {
   // useRef() is used to create an object that won't cause a re-render
   // when the value of obj.current is changed. By comparison, a string update would
