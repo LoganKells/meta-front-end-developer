8bd38c1e14452f1b0883a9a9c473ee5fc54d7e1a
LoganKells <lkells@outlook.com>
2023-10-19 06:22:41 -0400

    implemented Button and LoginButton components

--- course-6-advanced-react/lab-spread-operator-for-props/src/App.tsx
+++ course-6-advanced-react/lab-spread-operator-for-props/src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+
+interface ButtonProps {
+  buttonType: String;
+  children: React.ReactNode;
+  onClick: () => void;
+}
+
+const Button = ({ buttonType, children, ...buttonProps }: ButtonProps) => {
+  const className =
+    buttonType === "primary" ? "PrimaryButton" : "SecondaryButton";
+  return (
+    <button className={`Button ${className}`} {...buttonProps}>
+      {children}
+    </button>
+  );
+};
+
+const LoginButton = ({ buttonType, children, ...buttonProps }: ButtonProps) => {
+  return (
+    <Button
+      buttonType="secondary"
+      {...buttonProps}
+      onClick={() => alert("Logging in now!")}
+    >
+      {children}
+    </Button>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="Header">Login Page</header>
+      <Button buttonType="primary" onClick={() => alert("Signing up now!")}>
+        Sign up
+      </Button>
+      <LoginButton
+        buttonType="secondary"
+        onClick={() => alert("This is NOT displayed")}
+      >
+        Log in
+      </LoginButton>
     </div>
   );
 }
